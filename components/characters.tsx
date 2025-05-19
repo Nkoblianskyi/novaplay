2061437518c7bc272f982a6a92ea92a4271ae335
Nykyta Koblianskyi <nkoblianskyi@gmail.com>
2025-05-19 16:26:12 +0200

    up

--- components/characters.tsx
+++ components/characters.tsx
@@ -23,7 +23,7 @@ export function Characters() {
             GamCare
           </a>{" "}
           eller{" "}
-          <a href="https://gambleware.org" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.gambleaware.org/" target="_blank" rel="noopener noreferrer">
             GambleAware
           </a>{" "}
           for støtte og råd.
